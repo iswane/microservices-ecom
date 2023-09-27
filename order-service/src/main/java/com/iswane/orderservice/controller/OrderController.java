663b22b6e5d11e0180d9c9894fa8f006558b7805
Abdoulaye WANE <laye1wane@gmail.com>
2023-09-27 13:24:45 -0400

    Distributed tracing

--- order-service/src/main/java/com/iswane/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/iswane/orderservice/controller/OrderController.java
@@ -2,9 +2,6 @@ package com.iswane.orderservice.controller;
 
 import com.iswane.orderservice.dto.OrderRequest;
 import com.iswane.orderservice.service.OrderService;
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
-import io.github.resilience4j.retry.annotation.Retry;
-import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,11 +18,11 @@ public class OrderController {
     private final OrderService orderService;
 
     @PostMapping
-    @Retry(name = "inventory")
-    @TimeLimiter(name = "inventory")
-    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
-    public CompletableFuture<String> placeOrder(@RequestBody OrderRequest orderRequest) {
-        return CompletableFuture.supplyAsync(() -> orderService.placeOrder(orderRequest));
+    // @Retry(name = "inventory")
+    // @TimeLimiter(name = "inventory")
+    // @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
+    public String placeOrder(@RequestBody OrderRequest orderRequest) {
+        return orderService.placeOrder(orderRequest );
     }
 
     public CompletableFuture<String> fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException) {
