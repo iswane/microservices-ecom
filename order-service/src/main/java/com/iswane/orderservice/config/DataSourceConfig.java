e98a17524dfbf89c821578e5031382b94fa3790a
Abdoulaye WANE <laye1wane@gmail.com>
2023-10-14 14:01:08 -0400

    Dockerize app

--- order-service/src/main/java/com/iswane/orderservice/config/DataSourceConfig.java
+++ order-service/src/main/java/com/iswane/orderservice/config/DataSourceConfig.java
@@ -3,19 +3,21 @@ package com.iswane.orderservice.config;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import javax.sql.DataSource;
 
 @Configuration
+@Profile("dev")
 public class DataSourceConfig {
 
-    @Bean(name = "mysqlDataSource")
+    @Bean(name = "orderDataSource")
     public DataSource mysqlDataSource() {
         DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-        dataSourceBuilder.driverClassName("com.mysql.cj.jdbc.Driver");
-        dataSourceBuilder.url("jdbc:mysql://localhost:3307/order-service");
-        dataSourceBuilder.username("root");
-        dataSourceBuilder.password("mysql");
+        dataSourceBuilder.driverClassName("org.postgresql.Driver");
+        dataSourceBuilder.url("jdbc:postgresql://localhost:5431/order-service");
+        dataSourceBuilder.username("postgres");
+        dataSourceBuilder.password("postgres");
         return dataSourceBuilder.build();
     }
 }
